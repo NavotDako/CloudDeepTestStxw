1873c36a33b7ddcc1747f2385e22aaf149c1de9d
experitestayoub <32033577+experitestayoub@users.noreply.github.com>
2017-11-29 17:24:38 +0200

    Update Install.java

--- src/STXWActionTests/Install.java
+++ src/STXWActionTests/Install.java
@@ -1,6 +1,8 @@
 package STXWActionTests;
 
 import Utils.Utilities;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
@@ -12,10 +14,10 @@ public class Install extends BaseTest {
     @Test
     public void test() {
 
-        try {
-            Utilities.log(currentThread,"installlllll");
+        
             Utilities.log(currentThread, "Enter to Install testClass");
 
+            Utilities.sleep(currentThread, 2000);
             if (currentThread.STXWType.equals("manual")) {
                 driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item[1]/div/button[contains(@aria-label,'Install')]")).click();
                 Utilities.log(currentThread, "click on Install Button");
@@ -25,16 +27,14 @@ public class Install extends BaseTest {
             }
 
 
-            Thread.sleep(2000);
-            Utilities.log(currentThread, "wait 2 seconds");
+            Utilities.sleep(currentThread, 2000);
 
 
             driver.findElement(By.xpath("/html/body/div[2]/div/install-panel/div/md-list/md-list-item/div/button[contains(@aria-label,'ank')]")).click();
             Utilities.log(currentThread, "click on the Experibank application to install");
 
 
-            Thread.sleep(2000);
-            Utilities.log(currentThread, "wait 2 seconds");
+            Utilities.sleep(currentThread, 2000);
 
             if (currentThread.STXWType.equals("manual")) {
                 driver.findElement(By.xpath("/html/body/div[2]/div/install-panel/div/md-list/md-list-item/div/div[1]/div[contains(text(),'ank')]/md-card/md-card-actions/button")).click();
@@ -47,41 +47,38 @@ public class Install extends BaseTest {
             }
 
 
-            Thread.sleep(1000);
-            Utilities.log(currentThread, "wait 5 seconds");
+            Utilities.sleep(currentThread, 1000);
 
 
             driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div"));
             Utilities.log(currentThread, "test if application manager found");
 
 
-            Thread.sleep(30000);
-            Utilities.log(currentThread, "wait 30 seconds");
+            Utilities.sleep(currentThread, 30000);
 
 
             try {
                 text = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/div[5]/div/div/install-progress-panel/div/code")).getText();
             } catch (Exception e) {
                 Utilities.log(e);
+                throw e;
             }
             Utilities.log(currentThread, "get the Text in application manager (" + text + ")");
-            if (!(text.contains("Successfully") && text.contains("Starting"))) {
-
+            if (!(text.contains("Successfully") && text.contains("Starting"))) 
+            {
+                try {throw (new Exception("the install doesn't success"));}catch(Exception e) {}
             }
 
+            driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/md-toolbar/button[3]")).click();
+            Utilities.log(currentThread, "click on x (close the Install's text window)");
+            
             driver.findElement(By.xpath("/html/body/div[2]/div/install-panel/div/div[1]/button")).click();
             Utilities.log(currentThread, "click on x (close the Install window)");
 
-            Utilities.log(currentThread,"my text: " + text);
-        } catch (Exception e) {
-            Utilities.log(e);
-            Utilities.log(currentThread,e);
-
-        }
+            Utilities.sleep(currentThread, 3000);
+       
 
     }
 
 
 }
-
-
