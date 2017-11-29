04f6ad412828ed08894f9b4f492e71e38882530d
experitestayoub <32033577+experitestayoub@users.noreply.github.com>
2017-11-29 17:27:04 +0200

    Update Openlogs.java

--- src/STXWActionTests/Openlogs.java
+++ src/STXWActionTests/Openlogs.java
@@ -1,53 +1,56 @@
 package STXWActionTests;
 
 import Utils.Utilities;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
 public class Openlogs extends BaseTest {
 
-
+   
 
     @Test
     public void test() {
 
-        try {
+       
 
-            Utilities.log(currentThread,"openlogsssss");
             Utilities.log(currentThread, "Enter to Open Logs testClass");
-
+            Utilities.sleep(currentThread, 2000);
             driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item/div/button[contains(@aria-label,'Logs')]")).click();
             Utilities.log(currentThread, "click on open log Button ");
-
-
-                Thread.sleep(2000);
-                Utilities.log(currentThread, "wait 2 seconds");
-
+            
+                Utilities.sleep(currentThread, 2000);
 
             driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/md-toolbar"));
             Utilities.log(currentThread, "test if the logs open");
 
-
-                Thread.sleep(5000);
-                Utilities.log(currentThread, "wait 5 seconds");
+                Utilities.sleep(currentThread, 5000);
 
             try {
-                if (!driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/div[5]/div/div/log-panel/md-content/code/text()")).getText().equals(""))
+                if (!driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/div[5]/div/div/log-panel/md-content/code")).getText().equals(""))
 
                 {
-                    Utilities.log(currentThread, "the logs after 5 seconds" + driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/div[5]/div/div/log-panel/md-content/code/text()")).getText());
-                } else {
-
+                    Utilities.log(currentThread, "the logs after 5 seconds" + driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/div[5]/div/div/log-panel/md-content/code")).getText());
+                } 
+                else 
+                {
+                	Utilities.log(currentThread,"The logs are Empty");
+                    try {throw (new Exception("The logs are Empty"));}catch(Exception e) {}
                 }
+                
+                driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/md-toolbar/button[3]")).click();
+                Utilities.log(currentThread, "click on close logs");
+                Utilities.sleep(currentThread, 2000);
+                
+                
             } catch (Exception e) {
-                Utilities.log(e);
+                Utilities.log(currentThread,e.toString());
             }
-        } catch (Exception e) {
-            Utilities.log(currentThread,e.getMessage());
-        }
+      
 
     }
 
+   
 
 }
-// /html/body/div[2]/div/div[2]/div/div/div[5]/div/div/log-panel/md-content/code
