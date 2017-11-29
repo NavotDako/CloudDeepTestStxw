934f6e14452b20785e3f6636598e655fa3a0720e
experitestayoub <32033577+experitestayoub@users.noreply.github.com>
2017-11-29 17:25:28 +0200

    Update Monitors.java

--- src/STXWActionTests/Monitors.java
+++ src/STXWActionTests/Monitors.java
@@ -1,34 +1,51 @@
 package STXWActionTests;
 
 import Utils.Utilities;
+
+import java.util.Currency;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
 public class Monitors extends BaseTest {
 
+
     @Test
     public void test() {
 
 
-        try {
+      
 
-            Utilities.log(currentThread,"Monitorssssss");
             Utilities.log(currentThread, "Enter to monitors testClass");
 
-            driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item[1]/div/button[contains(@aria-label,'Monitors')]")).click();
-            Utilities.log(currentThread, "click on monitors Button");
+            Utilities.sleep(currentThread, 4000);
+            if(currentThread.STXWType.equals("manual")) 
+            {
+	            driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item[1]/div/button[contains(@aria-label,'Monitors')]")).click();
+	            Utilities.log(currentThread, "click on monitors Button");
+            }
+            else 
+            {
+            	driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item[1]/div/button[contains(@aria-label,'Network') or contains(@aria-label,'Monitors')]")).click();
+            	Utilities.log(currentThread, "click on Network Button");
+            }
+            Utilities.sleep(currentThread, 3000);
 
-            Thread.sleep(2000);
-            Utilities.log(currentThread, "wait 2 seconds");
+           try
+           {
+        	   driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div"));
+        	   Utilities.log(currentThread, "test if monitor popUp");
+           }
+           catch(Exception e) 
+           {
+        	   Utilities.log(currentThread, "monitor window doesn't popUp");               
+               try{throw new Exception("monitor window doesn't popUp");}catch(Exception e1) {Utilities.log(currentThread, "Throw Exception doesn't succeed");}
+           }
+           Utilities.log(currentThread, "monitor window is popUp");
 
-
-            driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div"));
-            Utilities.log(currentThread, "test if monitor popUp");
-
-        } catch (Exception e) {
-            Utilities.log(currentThread,e);
-
-        }
+       
     }
 
 
