ab184dd5757537f08795beb451385a9542c432bf
experitestayoub <32033577+experitestayoub@users.noreply.github.com>
2017-11-29 13:27:22 +0200

    Add files via upload

---
+++ SwipeAndClick.java
@@ -0,0 +1,24 @@
+package ActionTests;
+
+import Utils.Utilities;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SwipeAndClick extends BaseTest {
+
+
+
+    @Test
+    public void test() {
+        try {
+            Utilities.log(currentThread,"swipeandclickkk");
+            Utilities.log(currentThread, "Enter to swipeAndClick testClass");
+        } catch (Exception e) {
+            Utilities.log(currentThread,e);
+        }
+    }
+
+
+
+}
