e584251b94f8010786caeb834078d6cb6b328bca
experitestayoub <32033577+experitestayoub@users.noreply.github.com>
2017-11-30 13:20:32 +0200

    Update StartVideo.java

--- src/STXWActionTests/StartVideo.java
+++ src/STXWActionTests/StartVideo.java
@@ -1,7 +1,8 @@
-package ActionTests;
+package STXWActionTests;
 
 import Utils.Utilities;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -12,7 +13,7 @@ public class StartVideo extends BaseTest {
 	public void test() {
 		
 		
-		
+		try {
 			
 			Utilities.log(currentThread,"Enter to Start video testClass");
 			
@@ -42,92 +43,15 @@ public class StartVideo extends BaseTest {
 			catch(Exception e) 
 			{
 				Utilities.log(currentThread,"the video doesn't found");				
-				try{throw new Exception ("the video doesn't found");}catch(Exception e1) {}
+				Assert.fail("the video doesn't found");
 			}
 			Utilities.sleep(currentThread, 2000);
-	
+		}
+		catch(Exception e)
+		{
+			try{throw e;}catch(Exception e1) {Utilities.log(currentThread, "Throw Exception doesn't succeed");}
+		}
 		
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//package ActionTests;
-//
-//import Utils.Utilities;
-//import org.junit.Test;
-//import org.openqa.selenium.By;
-//
-//public class StartVideo extends BaseTest {
-//
-//    @Test
-//    public void test() {
-//
-//
-//        try {
-//            Utilities.log(currentThread, "start video");
-//            Utilities.log(currentThread, "Enter to Start video testClass");
-//
-//            driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item[2]/div/button[contains(@aria-label,'Video')]")).click();
-//            Utilities.log(currentThread, "click on satrt video Button ");
-//
-//
-//            Utilities.sleep(currentThread, 2000);
-//            Utilities.log(currentThread, "wait 2 seconds");
-//
-//            driver.findElement(By.xpath("/html/body/div[2]/div/video-recording-panel/div/div[2]/button")).click();
-//            Utilities.log(currentThread, "click on start");
-//
-//            driver.findElement(By.xpath("/html/body/div[2]/div/video-recording-panel/div/div[2]/button")).click();
-//            Utilities.log(currentThread, "click on stop");
-//        } catch (Exception e) {
-//            Utilities.log(currentThread, e);
-//        }
-//
-//    }
-//
-//}
