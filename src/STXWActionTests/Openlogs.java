29fe1c093be9a1262327180b365405bbdf9e8d5b
experitestayoub <32033577+experitestayoub@users.noreply.github.com>
2017-11-30 13:14:02 +0200

    Update Openlogs.java

--- src/STXWActionTests/Openlogs.java
+++ src/STXWActionTests/Openlogs.java
@@ -1,6 +1,8 @@
-package ActionTests;
+package STXWActionTests;
 
 import Utils.Utilities;
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,7 +15,7 @@ public class Openlogs extends BaseTest {
     @Test
     public void test() {
 
-        try {
+       
 
             Utilities.log(currentThread, "Enter to Open Logs testClass");
             Utilities.sleep(currentThread, 2000);
@@ -35,8 +37,8 @@ public class Openlogs extends BaseTest {
                 } 
                 else 
                 {
-                	Utilities.log(currentThread,"the logs are Empty");
-                    try {throw (new Exception("the logs are Empty"));}catch(Exception e) {}
+                	Utilities.log(currentThread,"The logs are Empty");
+                	Assert.fail("The logs are Empty");
                 }
                 
                 driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/md-toolbar/button[3]")).click();
@@ -47,113 +49,10 @@ public class Openlogs extends BaseTest {
             } catch (Exception e) {
                 Utilities.log(currentThread,e.toString());
             }
-        } catch (Exception e) {
-            Utilities.log(currentThread,e.getMessage());
-            try{throw e;}catch(Exception e1) {Utilities.log(currentThread, "Throw Exception doesn't succeed");}
-        }
+      
 
     }
 
-    @After
-    public void finsh() {
-        super.finish();
-    }
+   
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//package ActionTests;
-//
-//import Utils.Utilities;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.openqa.selenium.By;
-//
-//public class Openlogs extends BaseTest {
-//
-//
-//
-//    @Test
-//    public void test() {
-//
-//        try {
-//
-//            Utilities.log(currentThread,"openlogsssss");
-//            Utilities.log(currentThread, "Enter to Open Logs testClass");
-//
-//            driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item/div/button[contains(@aria-label,'Logs')]")).click();
-//            Utilities.log(currentThread, "click on open log Button ");
-//
-//
-//                Thread.sleep(2000);
-//                Utilities.log(currentThread, "wait 2 seconds");
-//
-//
-//            driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/md-toolbar"));
-//            Utilities.log(currentThread, "test if the logs open");
-//
-//
-//                Thread.sleep(5000);
-//                Utilities.log(currentThread, "wait 5 seconds");
-//
-//            try {
-//                if (!driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/div[5]/div/div/log-panel/md-content/code/text()")).getText().equals(""))
-//
-//                {
-//                    Utilities.log(currentThread, "the logs after 5 seconds" + driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/div[5]/div/div/log-panel/md-content/code/text()")).getText());
-//                } else {
-//
-//                }
-//            } catch (Exception e) {
-//                Utilities.log(e);
-//            }
-//        } catch (Exception e) {
-//            Utilities.log(currentThread,e.getMessage());
-//        }
-//
-//    }
-//
-//
-//}
