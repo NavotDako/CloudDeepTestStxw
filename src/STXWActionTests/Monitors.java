cd62e3058d9b23353aaef7fef296736d7e702d28
experitestayoub <32033577+experitestayoub@users.noreply.github.com>
2017-11-30 13:15:11 +0200

    Update Monitors.java

--- src/STXWActionTests/Monitors.java
+++ src/STXWActionTests/Monitors.java
@@ -1,10 +1,11 @@
-package ActionTests;
+package STXWActionTests;
 
 import Utils.Utilities;
 
 import java.util.Currency;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -16,12 +17,12 @@ public class Monitors extends BaseTest {
     public void test() {
 
 
-        try {
+      
 
             Utilities.log(currentThread, "Enter to monitors testClass");
 
             Utilities.sleep(currentThread, 4000);
-            if(currentThread.STXW.equals("manual")) 
+            if(currentThread.STXWType.equals("manual")) 
             {
 	            driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item[1]/div/button[contains(@aria-label,'Monitors')]")).click();
 	            Utilities.log(currentThread, "click on monitors Button");
@@ -41,100 +42,12 @@ public class Monitors extends BaseTest {
            catch(Exception e) 
            {
         	   Utilities.log(currentThread, "monitor window doesn't popUp");               
-               try{throw new Exception("monitor window doesn't popUp");}catch(Exception e1) {Utilities.log(currentThread, "Throw Exception doesn't succeed");}
+               Assert.fail("monitor window doesn't popUp");
            }
            Utilities.log(currentThread, "monitor window is popUp");
 
-        } 
-        catch (Exception e) 
-        {            
-            try{throw e;}catch(Exception e1) {Utilities.log(currentThread, "Throw Exception doesn't succeed");}
-        }
+       
     }
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//package ActionTests;
-//
-//import Utils.Utilities;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.openqa.selenium.By;
-//
-//public class Monitors extends BaseTest {
-//
-//    @Test
-//    public void test() {
-//
-//
-//        try {
-//
-//            Utilities.log(currentThread,"Monitorssssss");
-//            Utilities.log(currentThread, "Enter to monitors testClass");
-//
-//            driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item[1]/div/button[contains(@aria-label,'Monitors')]")).click();
-//            Utilities.log(currentThread, "click on monitors Button");
-//
-//            Thread.sleep(2000);
-//            Utilities.log(currentThread, "wait 2 seconds");
-//
-//
-//            driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div"));
-//            Utilities.log(currentThread, "test if monitor popUp");
-//
-//        } catch (Exception e) {
-//            Utilities.log(currentThread,e);
-//
-//        }
-//    }
-//
-//
-//}
