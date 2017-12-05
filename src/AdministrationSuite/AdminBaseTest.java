bd576da46f7bb70061e3cb9b97825dada9a72763
experitestayoub <32033577+experitestayoub@users.noreply.github.com>
2017-12-05 14:19:57 +0200

    Update AdminBaseTest.java

--- src/AdministrationSuite/AdminBaseTest.java
+++ src/AdministrationSuite/AdminBaseTest.java
@@ -1,92 +1,160 @@
-package AdministrationSuite;
+package Administration;
 
-import MyMain.BaseBaseTest;
+import static org.junit.Assert.*;
+
+import java.net.MalformedURLException;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import MyMain.STXWRunner;
 import Utils.Utilities;
 
-public abstract class AdminBaseTest extends BaseBaseTest{
+public abstract class BaseTest {
 
-
-    @Before
-    public void setUp() throws Exception {
-        runner = (AdminRunner) Thread.currentThread();
-        System.out.println("-----------------------------" + runner.getName() + " Starting A New Test!-----------------------------");
-        try {
-            driver = createDriver();
-            needToQuitDriverOnFinish = true;
+	STXWRunner currentThread = (STXWRunner) Thread.currentThread();
+	WebDriver driver;
+	private boolean needToQuitDriverOnFinish = false;
+	
+	
+	
+	
+	
+	@Before
+	public void setUp()throws Exception{
+		
+		System.out.println("-----------------------------" + currentThread.getName() + " Starting A New Test!-----------------------------");
+		try {
+			driver = createDriver();
+			needToQuitDriverOnFinish = true;
             LoginInToCloud();
 
-        } catch (Exception e) {
-            Utilities.log(runner, e);
-            Utilities.log(runner, "SETUP FOR - " + Thread.currentThread().getName() + " HAS FAILED!!!");
-            throw e;
-        }
+		
+		} catch (Exception e)
+		  {
+	            Utilities.log(currentThread, e);
+	            Utilities.log(currentThread, "SETUP FOR - " + Thread.currentThread().getName() + " HAS FAILED!!!");
+	            throw e;
+	      }
+		
+	}
+	
+	
 
-    }
-
-    @Test
-    abstract public void test();
-
-    @After
+	
+	@Test
+	abstract public void test(); 
+		
+	@After
     public void finish() {
-        Utilities.log(runner, "finish");
-		driver.quit();
-        Utilities.log(runner, "driver.quit");
-    }
+		Utilities.log(currentThread, "finish");
+//		driver.quit();
+//        Utilities.log(currentThread, "driver.quit");
+	}
+	
+	
+	
+	 private WebDriver createDriver() throws MalformedURLException {
 
-    private void LoginInToCloud() {
+	        Utilities.log(currentThread, "connect to selenium hub");
 
-        driver.get(runner.enums.hostName + "/index.html#/login");
-        Utilities.log(runner, "go to " + runner.enums.hostName + "/index.html#/login");
-        driver.findElement(By.name("username")).sendKeys("ayouba");
-        Utilities.log(runner, "Write username (ayouba)");
+	        Utilities.log(currentThread, "choose chrome capabilities");
 
-        driver.findElement(By.name("password")).sendKeys("Experitest2012");
-        Utilities.log(runner, "write the password ");
+	        DesiredCapabilities dc = new DesiredCapabilities().chrome();
+	        dc.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+	        ChromeOptions chromeOption = new ChromeOptions();
+	        chromeOption.addArguments("--start-maximized");
+	        dc.setCapability(ChromeOptions.CAPABILITY, chromeOption);
 
-        driver.findElement(By.name("login")).click();
-        Utilities.log(runner, "click on login");
+	        return new ChromeDriver(dc);
+	    }
+	 
+	    private void LoginInToCloud() {
+	    	
+	        driver.get(currentThread.enums.hostName + "/index.html#/login");
+	        Utilities.log(currentThread, "go to " + currentThread.enums.hostName + "/index.html#/login");
+	        driver.findElement(By.name("username")).sendKeys("ayouba");
+	        Utilities.log(currentThread, "Write username (ayouba)");
 
-    }
+	        driver.findElement(By.name("password")).sendKeys("Experitest2012");
+	        Utilities.log(currentThread, "write the password ");
 
-    public boolean WaitForElement(String xPath) {
+	        driver.findElement(By.name("login")).click();
+	        Utilities.log(currentThread, "click on login");
 
-        boolean needToWaitToElement = true;
-        long startWaitTime = System.currentTimeMillis();
+	    }
+	    public boolean WaitForElement(String xPath) {
 
-        while (needToWaitToElement && (System.currentTimeMillis() - startWaitTime) < 60000) {
-            try {
-                driver.findElement(By.xpath(xPath));
-                needToWaitToElement = false;
-            } catch (Exception e) {
-                Utilities.log(runner, "waiting for Element - " + xPath);
-                Utilities.sleep(runner, 1000);
-            }
+	        boolean needToWaitToElement = true;
+	        long startWaitTime = System.currentTimeMillis();
 
-        }
-        return !needToWaitToElement;
+	        while (needToWaitToElement && (System.currentTimeMillis() - startWaitTime) < 60000) {
+	            try {
+	                driver.findElement(By.xpath(xPath));
+	                needToWaitToElement = false;
+	            } catch (Exception e) {
+	                Utilities.log(currentThread, "waiting for Element - " + xPath);
+	                Utilities.sleep(currentThread, 1000);
+	            }
 
-    }
+	        }
+	        return !needToWaitToElement;
 
-    public boolean WaitForText(String xPath, String Text) {
-        boolean needToWaitToText = true;
-        long startWaitTime = System.currentTimeMillis();
+	    }
+	    
+	    void waitForLoad(WebDriver driver) {
+	        new WebDriverWait(driver, 30).until(wd ->
+	                ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
+	    }
+	    
+	    public boolean WaitForText(String xPath, String Text) 
+	    {
+	    	   boolean needToWaitToText = true;
+		        long startWaitTime = System.currentTimeMillis();
 
-        while (needToWaitToText && (System.currentTimeMillis() - startWaitTime) < 60000) {
-            try {
-                if (driver.findElement(By.xpath(xPath)).getText().contains(Text))
-                    needToWaitToText = false;
-            } catch (Exception e) {
-                Utilities.log(runner, "waiting for Text - " + Text);
-                Utilities.sleep(runner, 1000);
-            }
+		        while (needToWaitToText && (System.currentTimeMillis() - startWaitTime) < 60000) {
+		            try {
+		                if(driver.findElement(By.xpath(xPath)).getText().contains(Text))
+		                needToWaitToText = false;
+		            } catch (Exception e) {
+		                Utilities.log(currentThread, "waiting for Text - " + Text);
+		                Utilities.sleep(currentThread, 1000);
+		            }
 
-        }
-        return !needToWaitToText;
-    }
+		        }
+		        return !needToWaitToText;
+	    }
+	    
+	    private String watchedLog;
+	    
+	    @Rule
+	    public TestWatcher watchman = new TestWatcher() {
+	        @Override
+	        protected void failed(Throwable e, Description description) {
+	            watchedLog += description + "\n";
+	            Utilities.log(currentThread, Thread.currentThread().getName() + " FAILED !!!" + watchedLog);
+	            Utilities.log(currentThread, (Exception) e);
+	            Utilities.log(currentThread, "TEST HAS FAILED!!!");
+	        }
+
+	        @Override
+	        protected void succeeded(Description description) {
+	            watchedLog += description + " " + "success!\n";
+	            Utilities.log(currentThread, Thread.currentThread().getName() + " PASSED!!!" + watchedLog);
+	            Utilities.log(currentThread, "TEST HAS PASSED!!!");
+	        }
+	    };
 
 }
