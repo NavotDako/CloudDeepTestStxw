c71a4bc83fecfa49ede476ce51776e666cda512f
NavotDako <Navot@experitest.com>
2017-12-10 17:15:25 +0200

    done first

--- src/STGridSuite/STGridBaseTest.java
+++ src/STGridSuite/STGridBaseTest.java
@@ -6,6 +6,7 @@ import Utils.Utilities;
 import com.experitest.client.Client;
 import com.experitest.client.GridClient;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -26,16 +27,23 @@ public abstract class STGridBaseTest extends BaseBaseTest {
     public void SetUp() throws Exception {
         runner = (STGridRunner) Thread.currentThread();
         Utilities.log(runner, "Enter to setUp");
+        try{
 
         grid = new GridClient("ayoubadmindeeptest1"/*runner.user*/, "Experitest2012", "Default", Main.cs.HOST, Main.cs.PORT, Main.cs.SECURED);
-
         client = grid.lockDeviceForExecution(runner.testName, "@os='" + ((STGridRunner) runner).deviceOS + "'", 5, 30000);
+        }catch (Exception e){
+            Utilities.log(runner, "Can't create a grid client!!!");
+            Utilities.log(runner,e);
+            Assert.fail(e.getMessage());
+        }
+
         deviceName = client.getDeviceProperty("device.name");
         deviceOS = ((STGridRunner) runner).deviceOS;
         client.setReporter("xml", Main.logsFolder.getAbsolutePath() + "/STGridRunner/reports", deviceName + " - " + deviceOS + " - " + runner.testName + "_" + (runner.iteration + 1));
         chosenDeviceName = deviceName;
         deviceOSVersion = client.getDeviceProperty("device.version");
         deviceSN = client.getDeviceProperty("device.sn");
+        client.deviceAction("unlock");
     }
 
     @Test
@@ -43,6 +51,7 @@ public abstract class STGridBaseTest extends BaseBaseTest {
 
     @After
     public void finish() {
+
         try {
             client.generateReport(false);
             client.releaseClient();
