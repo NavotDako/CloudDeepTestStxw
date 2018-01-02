7d0d708db83dd64fcc615f32986ef2366502acc8
experitestayoub <32033577+experitestayoub@users.noreply.github.com>
2018-01-02 17:29:02 +0200

    Update Install.java

--- src/STXWSuite/tests/Install.java
+++ src/STXWSuite/tests/Install.java
@@ -57,7 +57,7 @@ public class Install extends STXWBaseTest {
 
             waitForElement("/html/body/div[2]/div/div[2]/div/div/div[5]/div/div/install-progress-panel/div/code");
             WaitForText("/html/body/div[2]/div/div[2]/div/div/div[5]/div/div/install-progress-panel/div/code", "Successfully");            
-            text = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/div[5]/div/div/install-progress-panel/div/code")).getText();
+            text = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/div[5]/div/div//div/code")).getText();
            
             Utilities.log(runner, "get the Text in application manager (" + text + ")");
             if (!(text.contains("Successfully") && text.contains("Starting"))) 
