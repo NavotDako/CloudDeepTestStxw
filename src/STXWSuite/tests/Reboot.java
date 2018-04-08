b1c14e58f4124493223264d1c267363a8f74ab3e
experitestayoub <32033577+experitestayoub@users.noreply.github.com>
2018-04-08 16:26:36 +0300

    Add files via upload

--- src/STXWSuite/tests/Reboot.java
+++ src/STXWSuite/tests/Reboot.java
@@ -16,6 +16,7 @@ import java.util.Base64;
 
 import org.json.JSONObject;
 import org.junit.*;
+import org.openqa.selenium.Dimension;
 
 public class Reboot extends STXWBaseTest {
 
@@ -23,195 +24,61 @@ public class Reboot extends STXWBaseTest {
     @Test
     public void test() {
 
-
         Utilities.log(runner, "Enter to Reboot testClass");
         Utilities.sleep(runner, 2000);
-        try
-        {
-        	driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item[2]/div/button[contains(@aria-label,'Reboot')]")).click();
-        }
-        catch(Exception e)
-        {
-        	driver.findElement(By.xpath("//*[@id='qReboot']/div/button")).click();
-        }
+        driver.findElement(By.xpath("//*[@id='action_reboot']")).click();
         Utilities.log(runner, "click on reboot button");
+
         Utilities.sleep(runner, 2000);
-        driver.findElement(By.xpath("/html/body/div[1]/div/div/reboot-confirm-dialog/div/div[3]/button[2]")).click();
+        driver.findElement(By.xpath("//button[span[contains(text(),'Confirm')]]")).click();
         Utilities.log(runner, "click on confirm Button");
 
         Utilities.sleep(runner, 4000);
-//        JSONObject deviceJson = null, deviceInfo = null;
-//
-//        try
-//        {
-//            deviceJson = getChosenDeviceJson(chosenDeviceName);
-//        } 
-//        catch (Exception e) {}
-//        
-//        deviceInfo = GetSpecificDevice(getStringFromJson(deviceJson, "id"));
-        String udid = "";
-//        udid = getStringFromJson(deviceInfo, "udid");
-//        Utilities.log(runner, "Device udid is " + udid);
-        
-        udid = driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div/div/device-loupe/div/div/div[1]/h4")).getText();
+        String udid = driver.findElement(By.xpath("//*[@id='device_sn']")).getAttribute("value");
         ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(newTab.get(0));
         Utilities.log(runner, "Go To devices tab");
         Utilities.sleep(runner, 3000);
-        
-        waitForElement("//*[@id='content-after-toolbar']/div/md-content[2]/div/div/div[1]/md-input-container[label[contains(text(),'Search')]]/input");
-        driver.findElement(By.xpath("//*[@id='content-after-toolbar']/div/md-content[2]/div/div/div[3]/md-menu/md-input-container[label[contains(text(),'Status')]]/div[textarea]/textarea")).click();
+
+        waitForElement("//*[label[contains(text(),'Search')]]/input");
+        driver.findElement(By.xpath("//md-input-container[*[contains(text(),'Status')]]/div[1]")).click();
         Utilities.log(runner, "Click on status textarea");
-        
+
         Utilities.sleep(runner, 2000);
-        waitForElement("//*[contains(@id,'menu_container')]/md-menu-content[md-menu-item[md-checkbox[contains(@aria-label,'available')]]]/section/button[span[contains(text(),'All')]]");
-        driver.findElement(By.xpath("//*[contains(@id,'menu_container')]/md-menu-content[md-menu-item[md-checkbox[contains(@aria-label,'available')]]]/section/button[span[contains(text(),'All')]]")).click();
+        waitForElement("//*[(contains(@id,'menu_container') and @aria-hidden='false')]//button[*[contains(text(),'All')]]");
+        driver.findElement(By.xpath("//*[(contains(@id,'menu_container') and @aria-hidden='false')]//button[*[contains(text(),'All')]]")).click();
         Utilities.log(runner, "Click on All button");
-        
-        driver.findElement(By.xpath("//*[@id='content-after-toolbar']/div/md-content[2]/div/div/div[1]/md-input-container[label[contains(text(),'Search')]]/input"));
+
+        driver.findElement(By.xpath("//*[label[contains(text(),'Search')]]/input"));
         Utilities.log(runner, "Click on Search input");
-        
+
         String status =  getStatus(udid);
         Utilities.log(runner, "Status is : " + status);
-        
+
         driver.switchTo().window(newTab.get(1));
-        Utilities.log(runner, "Go To device's tab");       
-        
-        if(!status.contains("In Use")) 
+        Utilities.log(runner, "Go To device's tab");
+
+        if(!status.contains("In Use"))
         {
-        	Assert.fail("Device status doesn't convert to In Use ");
+            Assert.fail("Device status doesn't convert to In Use ");
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//        boolean needToWait = true;
-//        long startTime = System.currentTimeMillis();
-//        while (needToWait && ((System.currentTimeMillis() - startTime) < 480000)) {
-//            deviceInfo = GetSpecificDevice(getStringFromJson(deviceJson, "id"));            
-//            if (getStringFromJson(deviceInfo, "currentStatus").equals("Offline") || getStringFromJson(deviceInfo, "currentStatus").equals("offline")) {
-//                needToWait = false;
-//            }
-//            Utilities.sleep(runner, 5000);
-//        }
-//
-//        Utilities.log(runner, "new device Info : " + deviceInfo);
-//        if (getStringFromJson(deviceInfo, "currentStatus").equals("Offline") || getStringFromJson(deviceInfo, "currentStatus").equals("offline")) {
-//            Utilities.log(runner, "the device's currentStatus is Offline");
-//            needToWait = true;
-//            startTime = System.currentTimeMillis();
-//            while (needToWait && ((System.currentTimeMillis() - startTime) < 480000)) {
-//
-//                deviceInfo = GetSpecificDevice(getStringFromJson(deviceJson, "id"));
-//                if (getStringFromJson(deviceInfo, "currentStatus").equals("online") || getStringFromJson(deviceInfo, "currentStatus").equals("Online")) {
-//                    needToWait = false;
-//                }
-//                Utilities.sleep(runner, 1000);
-//            }
-//            Utilities.log(runner, GetSpecificDevice(getStringFromJson(deviceJson, "id")).toString());
-//            if (!(getStringFromJson(GetSpecificDevice(getStringFromJson(deviceJson, "id")), "currentStatus").equals("online"))) {
-//                Utilities.log(runner, "the device doesn't goes to online case");
-//                Utilities.log(runner, "The displayStatus " + getStringFromJson(deviceInfo, "currentStatus"));
-//                Assert.fail("The device doesn't go to online case");
-//            } else {
-//                Utilities.log(runner, "the device's currentStatus is online");
-//            }
-//        } else {
-//        	deviceInfo = GetSpecificDevice(getStringFromJson(deviceJson, "id"));
-//        	Utilities.log(runner, "Device CurrentStatus is " + getStringFromJson(deviceInfo, "currentStatus"));
-//            Utilities.log(runner, "the device doesn't go to offline case");
-//            Assert.fail("the device doesn't go to offline case");
-//        }
+        Utilities.sleep(runner,10000);
 
     }
 
     private String getStatus(String udid) {
-    	
-    	driver.findElement(By.xpath("//*[@id='content-after-toolbar']/div/md-content[2]/div/div/div[1]/md-input-container[label[contains(text(),'Search')]]/input")).sendKeys(udid);
-    	Utilities.log(runner, "Write " + udid + " in search input ");
-		int count =0 ;
-    	Utilities.sleep(runner, 5000);
-    	while((!WaitForText("//*[@id='content-after-toolbar']/div/md-virtual-repeat-container/div/div[2]/div/md-content/table/tbody/tr/td[3]/div", "In Use")) || count < 8) 
-    	{
-    		Utilities.log(runner, "device current status: " + driver.findElement(By.xpath("//*[@id='content-after-toolbar']/div/md-virtual-repeat-container/div/div[2]/div/md-content/table/tbody/tr/td[3]/div")).getText());
-    		count++;
-    	}
-        return driver.findElement(By.xpath("//*[@id='content-after-toolbar']/div/md-virtual-repeat-container/div/div[2]/div/md-content/table/tbody/tr/td[3]/div")).getText();
-        
-	}
 
-	public String getStringFromJson(JSONObject jsonObject, String cell) {
-        String result = "";
-        try {
-            result = jsonObject.getString(cell);
-        } catch (Exception e) {
+        driver.findElement(By.xpath("//*[label[contains(text(),'Search')]]/input")).sendKeys(udid);
+        Utilities.log(runner, "Write " + udid + " in search input ");
+        int count = 0 ;
+        Utilities.sleep(runner, 5000);
+        while((!WaitForText("//table/tbody/tr/td[3]/div", "In Use")) && count < 8)
+        {
+            Utilities.log(runner, "device current status: " + driver.findElement(By.xpath("//table/tbody/tr/td[3]/div/span")).getText());
+            count++;
         }
+        return driver.findElement(By.xpath("//table/tbody/tr/td[3]/div/span")).getText();
 
-        return result;
     }
-
-
-    public JSONObject GetSpecificDevice(String deviceId) {
-        JSONObject JSONObject = null;
-        String DEVICES_URL = "/devices/";
-        String host = Main.cs.URL_ADDRESS;
-        String port = "";
-        String webPage = "" + host + "" + port + "/api/v1";
-        String authStringEnc;
-
-        String name = "ayouba";//TODO: admin testPlanUser name is here
-        String password = "Experitest2012";//TODO: admin password is here
-        String authString = name + ":" + password;
-        authStringEnc = Base64.getEncoder().encodeToString(authString.getBytes());
-        String getURL = prepareURL(DEVICES_URL, deviceId);
-        try {
-            return (JSONObject) (new JSONObject(doGet(getURL, webPage, authStringEnc)).get("data"));
-        } catch (Exception e) {
-        }
-        return JSONObject;
-    }
-
-    private String prepareURL(String DEVICES_URL, String deviceID) {
-        String getURL = DEVICES_URL + deviceID;
-        return getURL;
-    }
-
-    protected String doGet(String entity, String webPage, String authStringEnc) throws IOException {
-        URL url = new URL(webPage + entity);
-        URLConnection urlConnection = url.openConnection();
-        urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
-        InputStream is = urlConnection.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is);
-        int numCharsRead;
-        char[] charArray = new char[1024];
-        StringBuffer sb = new StringBuffer();
-        while ((numCharsRead = isr.read(charArray)) > 0) {
-            sb.append(charArray, 0, numCharsRead);
-        }
-        String result = sb.toString();
-        //   printGet(url, (HttpURLConnection) urlConnection, result);
-        boolean isResponseValid = ((HttpURLConnection) urlConnection).getResponseCode() < 300;
-        Assert.assertTrue("Did not get a valid response", isResponseValid);
-        return result;
-    }
-
 
 }
--- src/STXWSuite/tests/StartVideo.java
+++ src/STXWSuite/tests/StartVideo.java
@@ -14,25 +14,25 @@ public class StartVideo extends STXWBaseTest {
         Utilities.log(runner, "Enter to Start video testClass");
 
         Utilities.sleep(runner, 2000);
-        driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item[2]/div/button[contains(@aria-label,'Video')]")).click();
+        driver.findElement(By.xpath("//*[@id='video_recording_panel_openclose']")).click();
         Utilities.log(runner, "click on satrt video Button ");
 
         Utilities.sleep(runner, 2000);
 
-        driver.findElement(By.xpath("/html/body/div[2]/div/video-recording-panel/div/div[2]/button")).click();
+        driver.findElement(By.xpath("//video-recording-panel//button[*[contains(text(),'Start')]]")).click();
         Utilities.log(runner, "click on start");
 
         Utilities.sleep(runner, 5000);
 
-        driver.findElement(By.xpath("/html/body/div[2]/div/video-recording-panel/div/div[2]/button")).click();
+        driver.findElement(By.xpath("//video-recording-panel//button[*[contains(text(),'Stop')]]")).click();
         Utilities.log(runner, "click on stop");
 
         Utilities.sleep(runner, 3000);
 
         try {
-            driver.findElement(By.xpath("/html/body/div[2]/div/video-recording-panel/div/div[2]/div[2]"));
+            driver.findElement(By.xpath("//*[@id='device-plane-id']/video-recording-panel"));
             Utilities.log(runner, "Start video succeded");
-            driver.findElement(By.xpath("/html/body/div[2]/div/video-recording-panel/div/div[1]/button")).click();
+            driver.findElement(By.xpath("//video-recording-panel//button[*[contains(text(),'close')]]")).click();
             Utilities.log(runner, "click on close for video window");
         } catch (Exception e) {
             Utilities.log(runner, "the video doesn't found");
@@ -49,38 +49,3 @@ public class StartVideo extends STXWBaseTest {
 }
 
 
-//package ActionTests;
-//
-//import Utils.Utilities;
-//import org.junit.Test;
-//import org.openqa.selenium.By;
-//
-//public class StartVideo extends BaseTest {
-//
-//    @Test
-//    public void test() {
-//
-//
-//        try {
-//            Utilities.log(runner, "start video");
-//            Utilities.log(runner, "Enter to Start video testClass");
-//
-//            driver.findElement(By.xpath("//*[( contains(@id,'accordiongroup-') and contains(@id,'-panel'))]/div/md-list/md-list-item[2]/div/button[contains(@aria-label,'Video')]")).click();
-//            Utilities.log(runner, "click on satrt video Button ");
-//
-//
-//            Utilities.sleep(runner, 2000);
-//            Utilities.log(runner, "wait 2 seconds");
-//
-//            driver.findElement(By.xpath("/html/body/div[2]/div/video-recording-panel/div/div[2]/button")).click();
-//            Utilities.log(runner, "click on start");
-//
-//            driver.findElement(By.xpath("/html/body/div[2]/div/video-recording-panel/div/div[2]/button")).click();
-//            Utilities.log(runner, "click on stop");
-//        } catch (Exception e) {
-//            Utilities.log(runner, e);
-//        }
-//
-//    }
-//
-//}
